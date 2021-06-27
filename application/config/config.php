518b107df43fd99a29c66d3b910e3e5c7a333d10
thameemk612 <thameemk612@gmail.com>
2021-06-27 23:42:27 +0530

    gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$https = false;
+   if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+      $protocol = 'https://';
+    }
+    else {
+      $protocol = 'http://';
+    }
+
+$dirname = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/').'/';
+$root=$protocol.$_SERVER['HTTP_HOST'].$dirname;
+$config['base_url'] = $root;
 
 /*
 |--------------------------------------------------------------------------
