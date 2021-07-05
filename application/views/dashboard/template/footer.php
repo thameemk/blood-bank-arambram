187657d7bab6b07c2107edc7bce839d04ea16416
thameemk612 <thameemk612@gmail.com>
2021-07-05 16:48:44 +0530

    comlete prfile page

--- application/views/dashboard/template/footer.php
+++ application/views/dashboard/template/footer.php
@@ -6,7 +6,7 @@
 <script src="<?= base_url() ?>assets/vendors/popper.js/dist/umd/popper.min.js"></script>
 
 <script src="<?= base_url() ?>assets/vendors/jquery-validation/dist/jquery.validate.min.js"></script>
-<script src="<?= base_url() ?>assets/vendors/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.min.js"></script>
+<script src="<?= base_url() ?>/assets/vendors/jquery-validation-unobtrusive/src/jquery.validate.unobtrusive.js"></script>
 
 <script src="<?= base_url() ?>assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="<?= base_url() ?>assets/js/main.js"></script>
--- application/views/dashboard/template/sidebar.php
+++ application/views/dashboard/template/sidebar.php
@@ -21,9 +21,6 @@
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-
-
-
 </head>
 
 <body>
