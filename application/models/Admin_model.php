b1f5bc01e72ab031decd99cabfd3880f6da018e8
thameemk612 <thameemk612@gmail.com>
2021-07-08 14:04:59 +0530

    active donors

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -24,7 +24,7 @@ class Admin_model extends CI_Model
             'is_verified' => 1,
             'verified_admin' => $this->session->email
         );
-        $this->db->where('email', $this->session->email);
+        $this->db->where('email', $email);
         $this->db->update('users', $data);
         if ($this->db->affected_rows() == 1) {
             $response['status'] = true;
@@ -64,13 +64,45 @@ class Admin_model extends CI_Model
         return $response;
     }
 
+    function check_date($user_id)
+    {
+        date_default_timezone_set('Asia/Kolkata');
+        $today =  date('Y-m-d');
+        $this->db->select('user_id,donated_date');
+        $this->db->from('report');
+        $this->db->where('user_id', $user_id);
+        $this->db->order_by('donated_date', 'DESC');
+        $last_donation = $this->db->get()->row();
+        if ($last_donation != null) {
+            $last_date = $last_donation->donated_date;
+            $diff = abs(strtotime($today) - strtotime($last_date));
+            $years = floor($diff / (365 * 60 * 60 * 24));
+            $months = floor(($diff - $years * 365 * 60 * 60 * 24) / (30 * 60 * 60 * 24));
+            $days = floor(($diff - $years * 365 * 60 * 60 * 24 - $months * 30 * 60 * 60 * 24) / (60 * 60 * 24));
+            if ($days >= 90) {
+                return $last_donation;
+            }
+        } else {
+            $last_donation = array(
+                'user_id' => $user_id,
+                'donted_date' => null
+            );
+            return $last_donation;
+        }
+    }
+
     function get_all_active_donors()
     {
         $this->db->select('user_id,name,blood_group,phone,email,phone_2');
         $this->db->from('users');
-        $this->db->where('status',1);
-        $this->db->where('is_verified',1);
+        $this->db->where('status', 1);
+        $this->db->where('is_verified', 1);
         $query = $this->db->get();
+        // foreach ($query->result() as $user) {
+        //     $status = $this->check_date($user->user_id);
+        //     echo json_encode($status);
+        // }
+        // exit;
         return $query->result_array();
     }
 }
